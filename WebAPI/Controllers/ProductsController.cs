d107ad5f98b2610056f211bd81d301da35d02d09
Berkay ALGANER <39035340+berkayalganer@users.noreply.github.com>
2021-02-20 11:30:37 +0300

    1. Commit

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public string Get()
+        {
+            return "Merhaba";
+        }
+    }
+}
