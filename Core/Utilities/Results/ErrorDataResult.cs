eeb36820880a693098714e11f39d190ec5328a13
Berkay ALGANER <39035340+berkayalganer@users.noreply.github.com>
2021-02-21 11:34:34 +0300

    Autofac implementasyonu yapıldı.

--- Core/Utilities/Results/ErrorDataResult.cs
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -6,7 +6,7 @@ namespace Core.Utilities.Results
 {
     public class ErrorDataResult<T> : DataResult<T>
     {
-        public ErrorDataResult(T data, string message) : (data, false, message)
+        public ErrorDataResult(T data, string message) : base(data, false, message)
         {
 
         }
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
 ﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -17,10 +20,38 @@ namespace WebAPI.Controllers
         {
             _productService = productService;
         }
-        [HttpGet]
-        public string Get()
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
-            return "Merhaba";
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add([FromBody]Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
