41527759b67c965930112bc437992a3702681b41
Berkay ALGANER <39035340+berkayalganer@users.noreply.github.com>
2021-03-21 01:06:22 +0300

    CORS ayarları eklendi.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -49,11 +49,6 @@ namespace Business.Concrete
         [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
-            if(DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
-
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
